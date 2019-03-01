2bfc2dd1625c1129dc39a8bc3ce2fce0b62a80e0
ludiazoctavio <ludiazoctavio@ideapad-320.huawei.net>
2019-02-28 22:22:54 -0600

    Config react app and webpack

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Card from './src/dashboard/components/card';
+
+const app = document.getElementById('app')
+
+render(<Card/>,app);
---
+++ src/dashboard/components/card.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Card extends {Component} {
+    render() {
+        return (
+            <div>
+                <div>
+                    <img
+                        src=""
+                        alt=""
+                        width={260}
+                        height={160}
+                    />
+                </div>
+                <h3>$15,000 MXN</h3>
+                <p>Santa Maria la Ribera 96 C302 Santa María la Ribera, Cuauhtemoc CDMX</p>
+                <p>Octavio Diaz 5560076320</p>
+            </div>
+        )
+    }
+}
+
+export default Card;
